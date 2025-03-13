e5b2d09d1db9d99272c6889857fb5f2273993bfa
patrick <3423564182@qq.com>
2025-03-13 17:05:20 +0800

    feat(log): 添加日志处理

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod config;
+mod log;
 
 use std::collections::HashMap;
 use std::sync::Arc;
+use ::log::{error, info};
 use tokio::sync::Mutex;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use config::reader::reader;
@@ -12,17 +14,27 @@ async fn main() {
     let config = match reader() {
         Some(c) => c,
         None => {
-            println!("读取配置文件失败");
+           error!("读取配置文件失败");
             std::process::exit(1);
         }
     };
+
+    info!("读取配置文件成功");
+
+    // 初始化日志库
+    if let Err(e) =log::init::setup_logger(){
+        error!("初始化日志库失败,{}",e);
+        std::process::exit(1);
+    }
+
+    info!("初始化日志库成功");
 
     let listener = tokio::net::TcpListener::bind(config.rudis.address).await.unwrap();
     let hash_table: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
-        println!("Accepted connection from: {}", addr);
+        info!("接受 {} 地址的连接", addr);
 
         // 克隆 Arc 传入 tokio::spawn 线程
         let hash_table = Arc::clone(&hash_table);
@@ -39,12 +51,11 @@ async fn handle_client(socket: &mut tokio::net::TcpStream, hash_table: Arc<Mutex
         let mut buffer = [0; 1024];
         match socket.read(&mut buffer).await {
             Ok(0) => {
-                println!("客户端关闭");
+                info!("客户端关闭");
                 return;
             },
             Ok(_) => {
                 let command = String::from_utf8_lossy(&buffer[..]);
-                println!("接受客户端的消息: {}", command);
                 let command = command.to_lowercase();
 
                 // 分割命令字符串
@@ -54,14 +65,14 @@ async fn handle_client(socket: &mut tokio::net::TcpStream, hash_table: Arc<Mutex
                     Some(&"set") => handle_set_command(parts, socket, hash_table.clone()).await,
                     Some(&"get") => handle_get_command(parts, socket, hash_table.clone()).await,
                     _ => {
-                        println!("未定义的指令类型");
+                        error!("未定义的指令类型");
                         let response = "未定义的指令类型";
                         socket.write_all(response.as_bytes()).await.unwrap();
                     }
                 }
             },
             Err(e) => {
-                eprintln!("从服务端读取消息失败: {}", e);
+               error!("从服务端读取消息失败,{}",e);
                 return;
             }
         }
@@ -71,7 +82,7 @@ async fn handle_client(socket: &mut tokio::net::TcpStream, hash_table: Arc<Mutex
 // 处理 SET 命令
 async fn handle_set_command(parts: Vec<&str>, socket: &mut tokio::net::TcpStream, hash_table: Arc<Mutex<HashMap<String, String>>>) {
     if parts.len() != 3 {
-        println!("命令格式不符合！");
+        error!("命令格式不符合！");
         socket.write_all("命令格式不符合！".as_bytes()).await.unwrap();
         return;
     }
@@ -79,13 +90,7 @@ async fn handle_set_command(parts: Vec<&str>, socket: &mut tokio::net::TcpStream
     let key = parts[1].trim_end_matches('\0').to_string();
     let value = parts[2].trim_end_matches('\0').to_string();
 
-    println!("要存储的key是 : {}", key);
-    println!("要存储的value是 : {}", value);
-
     hash_table.lock().await.insert(key, value);
-
-    // 查看当前存储状态
-    println!("当前存储状态: {:#?}", hash_table.lock().await);
 
     socket.write_all("ok".as_bytes()).await.unwrap();
 }
@@ -93,7 +98,7 @@ async fn handle_set_command(parts: Vec<&str>, socket: &mut tokio::net::TcpStream
 // 处理 GET 命令
 async fn handle_get_command(parts: Vec<&str>, socket: &mut tokio::net::TcpStream, hash_table: Arc<Mutex<HashMap<String, String>>>) {
     if parts.len() != 2 {
-        println!("命令格式不符合！");
+        error!("命令格式不符合！");
         socket.write_all("命令格式不符合！".as_bytes()).await.unwrap();
         return;
     }
@@ -104,7 +109,7 @@ async fn handle_get_command(parts: Vec<&str>, socket: &mut tokio::net::TcpStream
     let value = match lock_hash.get(key) {
         Some(v) => v,
         None => {
-            println!("未找到key: {}", key);
+            error!("未找到key");
             socket.write_all("未找到key".as_bytes()).await.unwrap();
             return;
         }
